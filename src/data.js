d1d256aecce4d55910874019e8b325638877fce4
Godwin Yankey <36565894+YankeyTech@users.noreply.github.com>
2026-05-09 02:55:07 +0000

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const drinks = [
+  {
+    id: 1,
+    name: "Neon Paradise",
+    price: 120,
+    image:
+      "https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b"
+  },
+  {
+    id: 2,
+    name: "Blue Ocean Cocktail",
+    price: 95,
+    image:
+      "https://images.unsplash.com/photo-1470337458703-46ad1756a187"
+  }
+];
+
+export const foods = [
+  {
+    id: 3,
+    name: "Seafood Fried Rice",
+    price: 90,
+    image:
+      "https://images.unsplash.com/photo-1603133872878-684f208fb84b"
+  },
+  {
+    id: 4,
+    name: "Burger Deluxe",
+    price: 65,
+    image:
+      "https://images.unsplash.com/photo-1550547660-d9450f859349"
+  }
+];

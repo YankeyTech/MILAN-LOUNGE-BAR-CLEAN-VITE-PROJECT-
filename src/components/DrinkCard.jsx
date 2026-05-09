f89b9717ba5440717350c91b70aeb9b19b784c8b
Godwin Yankey <36565894+YankeyTech@users.noreply.github.com>
2026-05-09 02:57:20 +0000

    Create DrinkCard.jsx

---
+++ src/components/DrinkCard.jsx
@@ -0,0 +1,52 @@
+function DrinkCard({ item }) {
+
+  const addToCart = () => {
+
+    const cart =
+      JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push(item);
+
+    localStorage.setItem(
+      "cart",
+      JSON.stringify(cart)
+    );
+
+    alert("Added to cart");
+  };
+
+  return (
+    <div className="card">
+
+      <img src={item.image} />
+
+      <div className="card-content">
+
+        <h3>{item.name}</h3>
+
+        <p>GH₵{item.price}</p>
+
+        <div className="button-group">
+
+          <button onClick={addToCart}>
+            Add To Cart
+          </button>
+
+          <button
+            className="secondary"
+            onClick={() =>
+              window.open(
+                "https://wa.me/233241671158"
+              )
+            }
+          >
+            Order Now
+          </button>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DrinkCard;
